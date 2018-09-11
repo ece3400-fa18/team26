df6afacef35938c661e80f11b1d4a01842dd7250
nat <nl356@cornell.edu>
2018-09-10 23:09:06 -0400

    changes to lab 1 code to test servo

--- lab1/part6/part6.ino
+++ lab1/part6/part6.ino
@@ -8,16 +8,22 @@ int pos = 0;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  servo_L.attach(3);
-  servo_R.attach(4);
+  servo_L.attach(9);
+  servo_R.attach(10);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  servo_L.write(180);
+//  servo_L.write(180);
+//  servo_R.write(0);
+//  delay(3000);
+//  servo_L.write(180);
+//  servo_R.write(90);
+//  delay(3000);
+
+  //wanting to test individual servo...
   servo_R.write(0);
   delay(3000);
-  servo_R.write(90);
-  servo_L.write(180);
+  servo_R.write(45);
   delay(3000);
 }
