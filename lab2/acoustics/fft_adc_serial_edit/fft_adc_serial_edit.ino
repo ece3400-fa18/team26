42a6d909af19216c541d6e61e3348ef053c08be8
psk92cornell <psk92@cornell.edu>
2018-09-27 15:04:04 -0400

    fft code added

---
+++ lab2/acoustics/fft_adc_serial_edit/fft_adc_serial_edit.ino
@@ -0,0 +1,38 @@
+/*
+fft_adc_serial.pde
+guest openmusiclabs.com 7.7.14
+example sketch for testing the fft library.
+it takes in data on ADC0 (Analog0) and processes them
+with the fft. the data is sent out over the serial
+port at 115.2kb.
+*/
+
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
+#include <FFT.h> // include the library
+
+
+void setup() {
+  Serial.begin(115200); // use the serial port
+}
+
+void loop() {
+  while(1) {
+    cli();
+    for (int i = 0 ; i < 512 ; i += 2) {
+      fft_input[i] = analogRead(A1);
+      fft_input[i+1] = 0;
+    }
+    fft_window();
+    fft_reorder();
+    fft_run();
+    fft_mag_log();
+    sei();
+    Serial.println("start");
+    for (byte i = 0 ; i < FFT_N/2 ; i++) {
+      Serial.println(fft_log_out[i]);
+    }
+  }
+}
+
