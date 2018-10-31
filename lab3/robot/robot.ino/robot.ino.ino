f240d73c2aa1153f13fda0bb2f6e72dd635ccd30
sh2393 <sh2393@cornell.edu>
2018-10-30 23:13:41 -0400

    acoustic/robot integrated

--- lab3/robot/robot.ino/robot.ino.ino
+++ lab3/robot/robot.ino/robot.ino.ino
@@ -7,7 +7,11 @@ with the fft. the data is sent out over the serial
 port at 115.2kb.
 */
 
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
 #include <Servo.h>
+#include <FFT.h>
 
 Servo left_servo;
 Servo right_servo;
@@ -43,23 +47,13 @@ int   i = 0;
 int right_turn_val  = 0;
 int left_turn_val   = 0;
 
+const int irBinNum = 44;
+const int irThresh = 50;
+const int micBinNum = 17;
+const int micThresh = 130;
 
-void setup() {
-  Serial.begin(9600); // use the serial port
-  right_servo.attach(RW);
-  left_servo.attach(LW);
 
-  //line sensor
-  pinMode(right_turn, INPUT);
-  pinMode(left_turn, INPUT); 
-  pinMode(front_wall, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
 
-  right_servo.write(SERVO_BRAKE);
-  left_servo.write(SERVO_BRAKE);
-  delay(1000);
-  Serial.println("start");
-}
 
 void servos_stop(){
   Serial.println("stop");
@@ -138,51 +132,75 @@ void go_straight(){
   delay(10);
 }
 
-//int polling_ir(){
-//  cli();  // UDRE interrupt slows this way down on arduino1.0
-//  for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
-//    while(!(ADCSRA & 0x10)); // wait for adc to be ready
-//    ADCSRA = 0xf5; // restart adc (32 prescalar)
-//    byte m = ADCL; // fetch adc data
-//    byte j = ADCH;
-//    int k = (j << 8) | m; // form into an int
-//    k -= 0x0200; // form into a signed int
-//    k <<= 6; // form into a 16b signed int
-//    fft_input[i] = k; // put real data into even bins
-//    fft_input[i+1] = 0; // set odd bins to 0
-//  }
-//  fft_window(); // window the data for better frequency response
-//  fft_reorder(); // reorder the data before doing the fft
-//  fft_run(); // process the data in the fft
-//  fft_mag_log(); // take the output of the fft
-//  sei();
-//
-//  Serial.println(fft_log_out[irBinNum]);
-//
-//  if (fft_log_out[irBinNum] > irThresh) {
-//    Serial.println("ir");
-//    //digitalWrite(7, HIGH);
-//    digitalWrite(LED_BUILTIN, HIGH);
-//    return 1;
-//  } else {
-//    Serial.println("no ir");
-//    //digitalWrite(7, LOW);
-//    digitalWrite(LED_BUILTIN, LOW);
-//    return 0;
-//  }
-//}
+
 
 
 bool wallDetected(){
   int distance = analogRead(front_wall);
-  Serial.println(distance);
+  //Serial.println(distance);
   return (distance > 305);
 }
 
+void detectAudio(){
+  
+  byte prevTIMSK0 = TIMSK0;
+  byte prevADCSRA = ADCSRA;
+  byte prevADMUX = ADMUX;
+  byte prevDIDR0 = DIDR0;
+
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+  while(1){
+    Serial.println("while1");
+    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+      while (!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf7; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i + 1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei();
+    Serial.println(fft_log_out[micBinNum]);
+   if (fft_log_out[micBinNum] > micThresh){
+    Serial.println(fft_log_out[micBinNum]);
+    break;
+   } 
+  }
+  TIMSK0 = prevTIMSK0;
+  ADCSRA = prevADCSRA;
+  ADMUX = prevADMUX;
+  DIDR0 = prevDIDR0;
+}
+
+void setup() {
+  Serial.begin(9600); // use the serial port
+  detectAudio();
+  
+  right_servo.attach(RW);
+  left_servo.attach(LW);
+
+  //line sensor
+  pinMode(right_turn, INPUT);
+  pinMode(left_turn, INPUT); 
+  pinMode(front_wall, INPUT);
+}
+
 void loop() {
+//  right_servo.write(SERVO_BRAKE);
+//  left_servo.write(SERVO_BRAKE);
   go_straight();
   if (wallDetected()){
-    //find_intersection();
+    Serial.println("wall");
     move(right);
   }
 }
