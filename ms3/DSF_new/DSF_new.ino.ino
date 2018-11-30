bf02d92c74663596927482c7f3a7f5017c918a40
ut33 <ut33@cornell.edu>
2018-11-30 11:59:38 -0500

    Update DSF_new.ino.ino

--- ms3/DSF_new/DSF_new.ino.ino
+++ ms3/DSF_new/DSF_new.ino.ino
@@ -1,3 +1,66 @@
+#define LOG_OUT 1 // use the log output function
+#define FFT_N 256 // set to 256 point fft
+
+#include <Servo.h>
+#include <FFT.h>
+
+Servo left_servo;
+Servo right_servo;
+
+/***********************************************************************
+ *        Variable Declarations 
+ ***********************************************************************/
+
+// Pin Assignments 
+const int LW = 5; // Servo1
+const int RW = 6; // Servo2
+int right_turn = A5;
+int left_turn  = A4;
+int front_wall = A2;
+int left_wall =  A3;
+
+#define SERVO_BRAKE          90
+#define SERVO_L_FORWARD_MAX  95.0
+#define SERVO_R_FORWARD_MAX  85.0
+#define SERVO_L_INCR_FORWARD 2.0
+#define SERVO_R_INCR_FORWARD -2.0
+
+// Thresholds for each sensor to determine when over a line
+#define WHITE       700
+#define ERROR_RANGE 100
+#define left        0
+#define forward     1
+#define right       2
+
+// Control Variables for line following
+float error           = 0;
+float error_magnitude = 0;
+int   i = 0;
+
+// Line sensor values
+int right_turn_val  = 0;
+int left_turn_val   = 0;
+
+// Control variables for turning
+int turn_count = 0;
+const int irBinNum  = 44;
+const int irThresh  = 50;
+const int micBinNum = 17;
+const int micThresh = 135;
+
+//// Location and Direction values
+//volatile int unsigned long x = 0; //initial x 
+//volatile int unsigned long y = -1; //initial y 
+//int facing = 3; //initially facing NORTH
+//int direction = 0; //initially going straight
+//volatile int unsigned long d = 0;
+//int unsigned long updates[3] = {x,y,d};
+
+//Intersection Flag
+int interflag = 0;
+int updateFlag = 1;
+
+//DSF values
 int flag = 0; //corresponds to how many directions did not lead to a new position, used to decide direction
 int dir = 1; //pointer to direction || N = 1, E = 3, S = 2, W = 4
 /*
@@ -38,16 +101,128 @@ int dir = 1; //pointer to direction || N = 1, E = 3, S = 2, W = 4
  * else if (dir == W)
  *     B = A - 1;
 */
-array path[81] = {};
+int path[81] = {};
 int curpath = 0; //pointer to path, gives indice that corresponds to currentlocation in path
 int lastBranch = 0;//pointer to path, gives indice that corresponds to latest frontier
 int numTraversed = 0;
-array frontier[81] = {};
+int frontier[81] = {};
 int curfront = 0;//pointer to frontier
 
 /***********************************************************************
  *        Subroutines/Helper Functions
- ***********************************************************************/
+***********************************************************************/
+void servos_stop(){
+//  Serial.println("stop");
+  right_servo.write(SERVO_BRAKE);
+  left_servo.write(SERVO_BRAKE);
+  delay(3000);
+}
+
+void read_turn(){
+  right_turn_val = analogRead(right_turn); //signal from outer right sensor
+  left_turn_val = analogRead(left_turn);  //signal from outer left sensor 
+}
+
+void turn(int direction){
+  // Turn if requested
+    if(direction == right){
+      left_servo.write(95);
+      right_servo.write(95);
+      delay(50);
+      while(analogRead(left_turn) > WHITE);
+//      while(analogRead(left_turn) < WHITE);
+    }
+    
+    if(direction == left){
+      left_servo.write(85);
+      right_servo.write(85);
+      delay(50);
+      while(analogRead(right_turn) > WHITE);
+//      while(analogRead(right_turn) < WHITE);
+    }
+}
+
+void checkIntersection(){
+    if (left_turn_val < WHITE && right_turn_val < WHITE){
+      interflag = 1;
+    }
+    else {
+      interflag = 0;
+      updateFlag = 1;
+    }
+}
+
+
+void go_straight(){
+  checkIntersection();
+  //modified from solution code
+  //following straight line
+  read_turn();
+  error = left_turn_val - right_turn_val; // Positive position to right of line
+
+  // Correct robot's driving direction according to position error
+  if (abs(error) <= ERROR_RANGE){
+    left_servo.write(SERVO_L_FORWARD_MAX);
+    right_servo.write(SERVO_R_FORWARD_MAX);
+  }
+  // Robot is too right
+  else if (error > ERROR_RANGE) {
+    // Adjust left
+    error_magnitude = abs(error)/(float)ERROR_RANGE;
+    left_servo.write(SERVO_L_FORWARD_MAX + error_magnitude*SERVO_L_INCR_FORWARD);
+    right_servo.write(SERVO_R_FORWARD_MAX - error_magnitude*SERVO_R_INCR_FORWARD);
+  }
+  // Robot is too left of line
+  else if (error < -(ERROR_RANGE)) {
+    // Adjust right
+    error_magnitude = abs(error)/(float)ERROR_RANGE;
+    left_servo.write(SERVO_L_FORWARD_MAX - error_magnitude*SERVO_L_INCR_FORWARD);
+    right_servo.write(SERVO_R_FORWARD_MAX + error_magnitude*SERVO_R_INCR_FORWARD);
+  }
+
+  delay(10);
+}
+
+void detectAudio(){
+  byte prevTIMSK0 = TIMSK0;
+  byte prevADCSRA = ADCSRA;
+  byte prevADMUX = ADMUX;
+  byte prevDIDR0 = DIDR0;
+
+  TIMSK0 = 0; // turn off timer0 for lower jitter
+  ADCSRA = 0xe5; // set the adc to free running mode
+  ADMUX = 0x40; // use adc0
+  DIDR0 = 0x01; // turn off the digital input for adc0
+  while(1){
+    //Serial.println("while1");
+    for (int i = 0 ; i < 512 ; i += 2) { // save 256 samples
+      while (!(ADCSRA & 0x10)); // wait for adc to be ready
+      ADCSRA = 0xf7; // restart adc
+      byte m = ADCL; // fetch adc data
+      byte j = ADCH;
+      int k = (j << 8) | m; // form into an int
+      k -= 0x0200; // form into a signed int
+      k <<= 6; // form into a 16b signed int
+      fft_input[i] = k; // put real data into even bins
+      fft_input[i + 1] = 0; // set odd bins to 0
+    }
+    fft_window(); // window the data for better frequency response
+    fft_reorder(); // reorder the data before doing the fft
+    fft_run(); // process the data in the fft
+    fft_mag_log(); // take the output of the fft
+    sei();
+    //Serial.println(fft_log_out[micBinNum]);
+    if (fft_log_out[micBinNum] > micThresh){
+    //Serial.println(fft_log_out[micBinNum]);
+    break;
+   } 
+  }
+  TIMSK0 = prevTIMSK0;
+  ADCSRA = prevADCSRA;
+  ADMUX = prevADMUX;
+  DIDR0 = prevDIDR0;
+}
+ 
 bool frontdetectWall(){ //if no wall in front of robot, returns true 
   //sends radio of front wall
 }
@@ -58,7 +233,8 @@ bool rightdetectWall(){ //if no wall in front of robot, returns true
   //sends radio of walls on right side
 }
 
-int nextPos(int curpos, dir){
+int nextPos(int curpos){
+  int newpos = 0;
   if (curpos == 0){
     newpos = 1;
   }
@@ -77,46 +253,49 @@ int nextPos(int curpos, dir){
   return newpos; 
 }
 
-int sidePosL(int curpos, dir){
+int sidePosL(int curpos){
+  int pos = 0;
   if (dir == 1){
-    newpos = curpos -1;
+    pos = curpos -1;
   }
   else if(dir == 2){
-    newpos = curpos +1;
+    pos = curpos +1;
   }
   else if(dir == 3){
-    newpos = curpos +9;
+    pos = curpos +9;
   }
   else if(dir == 4){
-    newpos = curpos -9;
+    pos = curpos -9;
   }
   return pos;
  
 }
 
-int sidenextPosR(int curpos, dir){
+int sidePosR(int curpos){
+  int pos = 0;
   if (dir == 1){
-    newpos = curpos +1;
+    pos = curpos +1;
   }
   else if(dir == 2){
-    newpos = curpos -1;
+    pos = curpos -1;
   }
   else if(dir == 3){
-    newpos = curpos -9;
+    pos = curpos -9;
   }
   else if(dir == 4){
-    newpos = curpos +9;
+    pos = curpos +9;
   }
   return pos;
 }
 
-bool isNew(int pos, bool IsPath){ //if array is path, put 1, if frontier put 0, if new func returns true 
+bool isNew(int pos, bool IsPath){ //if array is path, put 1, if frontier put 0, if pos is new func returns true 
+  bool tf = 1;
   if(IsPath == 1){
     if(numTraversed == 0){
       tf = 1;
     }
     else {
-      for (i=0; i<=curpath; i++) {
+      for (int i=0; i<=curpath; i++) {
         if(path[i] == pos){
           tf = 0;
         }
@@ -128,7 +307,7 @@ bool isNew(int pos, bool IsPath){ //if array is path, put 1, if frontier put 0, 
       tf = 1;
     }
     else{
-      for (i=0; i<=curfront; i++) {
+      for (int i=0; i<=curfront; i++) {
         if(frontier[i] == pos){
           tf = 0;
           }
@@ -138,59 +317,59 @@ bool isNew(int pos, bool IsPath){ //if array is path, put 1, if frontier put 0, 
     return tf;
 }
 
-void newDir(int pos, dir){ //if all directions lead to old or blocked by wall returns -1
-  F = frontdetectWall();
-  R = rightdetectWall();
-  L = leftdetectWall();
-  n = nextPos(pos, 1);
-  s = nextPos(pos, 2);
-  e = nextPos(pos, 3);
-  w = nextPos(pos, 4);
+void newDir(int pos){ //if all directions lead to old or blocked by wall returns -1
+  bool F = frontdetectWall();
+  bool R = rightdetectWall();
+  bool L = leftdetectWall();
+  int n = pos + 9;
+  int s = pos - 9;
+  int e = pos + 1;
+  int w = pos - 1;
   flag = -1;//if all possibilities are either blocked by walls or already traversed
   if (dir == 1){
     if (R && isNew(e, 1)){
-      turn RIGHT
+      turn(right);
       dir = 3;
       flag = 0;
     }
     else if(L && isNew(w,1)){
-      turn LEFT;
+      turn(left);
       dir = 4;
       flag = 0;
     }
   }
   else if(dir == 2){
     if (R && isNew(w, 1)){
-      turn RIGHT
+      turn(right);
       dir = 4;
       flag = 0;
     }
     else if(L && isNew(e,1)){
-      turn LEFT;
+      turn(left);
       dir = 3;
       flag = 0;
     }
   }
   else if(dir == 3){
     if (R && isNew(s, 1)){
-      turn RIGHT
+      turn(right);
       dir = 2;
       flag = 0;
     }
-    else if(L && isNew(n,1){
-      turn LEFT;
+    else if(L && isNew(n,1)){
+      turn(left);
       dir = 1;
       flag = 0;
     }  
   }
   else if(dir == 4){
     if (R && isNew(n, 1)){
-      TURN RIGHT
+      turn(right);
       dir = 1;
       flag = 0;
     }
-    else if(L && isNew(s,1){
-      TURN LEFT;
+    else if(L && isNew(s,1)){
+      turn(left);
       dir = 2;
       flag = 0;
     }
@@ -198,14 +377,14 @@ void newDir(int pos, dir){ //if all directions lead to old or blocked by wall re
 }
 
 int isPossible(int pos, int NSEW){
-  ispos = 0; //default is not possible
-  F = frontdetectWall();
-  R = rightdetectWall();
-  L = leftdetectWall();
-  n = nextPos(pos, 1);
-  s = nextPos(pos, 2);
-  e = nextPos(pos, 3);
-  w = nextPos(pos, 4);
+  bool ispos = 0; //default is not possible
+  bool F = frontdetectWall();
+  bool R = rightdetectWall();
+  bool L = leftdetectWall();
+  int n = pos + 9;
+  int s = pos - 9;
+  int e = pos + 1;
+  int w = pos - 1;
   
   if (dir == 1){
     if (NSEW == 1){
@@ -291,10 +470,10 @@ int isPossible(int pos, int NSEW){
 }
 
 int findOldest(int pos){//returns oldest position from avaible options
-  n = 0;
-  s = 0; 
-  e = 0; 
-  w = 0; 
+  int n = 0;
+  int s = 0; 
+  int e = 0; 
+  int w = 0; 
   if (isPossible(pos, 1)){
     n = pos + 9;
   }
@@ -307,110 +486,110 @@ int findOldest(int pos){//returns oldest position from avaible options
   if (isPossible(pos, 4)){
     w = pos - 1;
   }
-  tot = [n,s,e,w];
-  for (i = 0; i < = 3;i++){
-    if (tot[i] ~= 0){
-      for (k=0; k<=curpath; k++) {
+  int tot[4] = {n,s,e,w};
+  for (int i = 0; i <= 3;i++){
+    if (tot[i] != 0){
+      for (int k=0; k<=curpath; k++) {
         if(path[k] == tot[i]){
           tot[i] = k;//k is index when that value was added
         }
       }
     }
-    elseif(tot[i] == 0){
+    else if(tot[i] == 0){
       tot[i] = 100;
     }
   }
 
   //get minimum value
-  mini = 100;
-  for (i = 0; i < = 3;i++){
+  int mini = 100;
+  for (int i = 0; i <= 3;i++){
     if(tot[i] < mini){
       mini = tot[i];
     }
   }
-
+  int oldest = pos + 9;
   if(tot[1] = mini){
-    newpos = pos + 9;
+    oldest = pos + 9;
   }
   else if(tot[2] = mini){
-    newpos = pos - 9;
+    oldest = pos - 9;
   }
   else if(tot[3] = mini){
-    newpos = pos + 1;
+    oldest = pos + 1;
   }
   else if(tot[4] = mini){
-    newpos = pos - 1;
+    oldest = pos - 1;
   }
-  return newpos; 
+  return oldest; 
 }
 
-void getDirnMove(int posa, int posb, dir){//gets direction between posa and posb, and turns accordingly
+void getDirnMove(int posa, int posb){//gets direction between posa and posb, and turns accordingly
   if(posb == posa + 9){//N
     if(dir == 1){
       //no need
     }
     else if(dir == 2){  
-      TURN RIGHT;
-      TURN RIGHT;
+      turn(right);
+      turn(right);
       dir = 1; 
     }
     else if(dir == 3){  
-      TURN LEFT;
+      turn(left);
       dir = 1;  
     }
     else if(dir == 4){ 
-      TURN RIGHT;
+      turn(right);
       dir = 1;  
     }    
   }
   else if(posb == posa - 9){//S
     if(dir == 1){
-      TURN RIGHT;
-      TURN RIGHT;
+      turn(right);
+      turn(right);
       dir = 2;
     }
     else if(dir == 2){  
        //no need 
     }
     else if(dir == 3){ 
-      TURN RIGHT;  
+      turn(right);  
       dir = 2;
     }
     else if(dir == 4){ 
-      TURN LEFT;
+      turn(left);
       dir = 2; 
     } 
   }
   else if(posb == posa + 1){//E
     if(dir == 1){
-      TURN RIGHT;
+      turn(right);
       dir = 3;
     }
     else if(dir == 2){ 
-      TURN LEFT;
+      turn(left);
       dir = 3;  
     }
     else if(dir == 3){ 
        //no need  
     }
     else if(dir == 4){ 
-      TURN RIGHT;
-      TURN RIGHT;
+      turn(right);
+      turn(right);
       dir = 3; 
     } 
   }
   else if(posb == posa - 1){//W
     if(dir == 1){
-      TURN LEFT;
+      turn(left);
       dir = 4;
     }
     else if(dir == 2){
-      TURN RIGHT;
+      turn(right);
       dir = 4;   
     }
     else if(dir == 3){
-      TURN RIGHT;
-      TURN RIGHT;
+      turn(right);
+      turn(right);
       dir = 4;   
     }
     else if(dir == 4){ 
@@ -420,27 +599,27 @@ void getDirnMove(int posa, int posb, dir){//gets direction between posa and posb
 }
 
 void moveDSF(){
-  curpos = path[curpath];//where you currently
-  branch = path[lastBranch];//the point you will traverse to
-  newpos = frontier[curfront];//the new point
+  int curpos = path[curpath];//where you currently
+  int branch = path[lastBranch];//the point you will traverse to
+  int newpos = frontier[curfront];//the new point
   //out avaible options find best for first move, move to it and set curpath pointer to it
-  nextloc = findOldest(curpos);
+  int nextloc = findOldest(curpos);
   getDirnMove(curpos, nextloc);
   checkIntersection();
-  while(~interflag){
-    move(forward);
+  while(!interflag){
+    go_straight();
   }
-  for (k=0; k<=curpath; k++) {
+  for (int k=0; k<=curpath; k++) {
       if(path[k] == nextloc){
         curpath = k;//k is index of current point
       }
   }
   //from that point, traverse backwards until branch is reached
-  for (k=curpath; k>=branch+1; k--) {
+  for (int k=curpath; k>=branch+1; k--) {
       getDirnMove(path[k], path[k-1]);
       checkIntersection();
-      while(~interflag){
-        move(forward);
+      while(!interflag){
+        go_straight();
       }
   }
   //after reaching branch reset curpath using numTraversed -1 
@@ -456,24 +635,31 @@ void moveDSF(){
 
 void setup() {
   // put your setup code here, to run once:
-
+//  Serial.begin(9600); // use the serial port
+  detectAudio();
+  right_servo.attach(RW);
+  left_servo.attach(LW);
+  pinMode(right_turn, INPUT);
+  pinMode(left_turn, INPUT); 
+  pinMode(front_wall, INPUT);
+  pinMode(left_wall, INPUT);
 }
 
 void loop() {
   //Start from (1,1). Search Order: N, E, S, W 
-  curpos = path[curpath];//robot starts from 0  
-  newpos = nextPos(curpos);
-  a = frontdetectWall();
-  b = isNew(newpos, 1);
+  int curpos = path[curpath];//robot starts from 0  
+  int newpos = nextPos(curpos);
+  bool a = frontdetectWall();
+  bool b = isNew(newpos, 1);
   //if no wall & next location new, advance
   if(a&&b){
     flag = 0;//robot can move forward 
-    if(curpos ~= 0){//as long as this is before the robot has reached the first intersection 
+    if(curpos != 0){//as long as this is before the robot has reached the first intersection 
       //check if walls on sides, if not, add to frontier
-      posL = sidePosL(curpos);
-      la = leftdetectWall();
-      lb = isNew(posL,0);//check not already in frontier
-      lc = isNew(posL,1);//check not already traversed
+      int posL = sidePosL(curpos);
+      bool la = leftdetectWall();
+      bool lb = isNew(posL,0);//check not already in frontier
+      bool lc = isNew(posL,1);//check not already traversed
       if(la && lb && lc){
         if(frontier[curfront]){
           frontier[curfront] = posL;
@@ -483,10 +669,10 @@ void loop() {
           frontier[curfront] = posL;
         }
       }
-      posR = sidePosR(curpos);
-      ra = rightdetectWall();
-      rb = isNew(posR,0);//check not already in frontier
-      rc = isNew(posR,1);//check not already traversed
+      int posR = sidePosR(curpos);
+      bool ra = rightdetectWall();
+      bool rb = isNew(posR,0);//check not already in frontier
+      bool rc = isNew(posR,1);//check not already traversed
       if(ra && rb && rc){
         if(frontier[curfront] == 0){
           frontier[curfront] = posR;
@@ -501,27 +687,27 @@ void loop() {
       }
     }
     //go forward in current direction 
-    move(forward);
+    go_straight();
     //when intersection found, add new position to path => should be in line following function
     checkIntersection();
-    if(interflag){
+    if(interflag && updateFlag){
       if(path[curpath] == 0){
        path[curpath] = newpos; 
-       numTraversed = numTraversed +1;
       }
       else {
         curpath = curpath +1;
         path[curpath] = newpos;
-        numTraversed = numTraversed +1;
       }
+      updateFlag = 0;
+      numTraversed = numTraversed +1;
     }
   }
   //if wall or next location old, check left and right walls and switch direction
-  else if (~(a&b)){
+  else if (!(a&b)){
     flag = 1;
   }
   //switch direction
-  if(flag ~= 0){
+  if(flag != 0){
     newDir(curpos); // turning taken care of inside function 
     if(flag == -1){
       //if all directions lead to old or blocked by wall
